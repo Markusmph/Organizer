18691c35d05cc00090fb1aaf27c542fcea33dcd5
Marco <marcomph@hotmail.com>
2020-03-16 21:57:52 -0600

    Instructions added. The user can add, display and edit assignments

--- Organizer.py
+++ Organizer.py
@@ -22,21 +22,9 @@ def get_input():
 def run_instruc(instruction):
     # noun, subj_index, assignm_index, value
     verb = instruction[0]
+    del instruction[0]
     if verb in verb_dict:
-        if len(instruction) == 1:
-            verb_dict[verb]()
-        elif len(instruction) == 2:
-            verb_dict[verb](instruction[1])
-        elif len(instruction) == 3:
-            verb_dict[verb](instruction[1], instruction[2])
-        elif len(instruction) == 4:
-            verb_dict[verb](instruction[1], instruction[2], instruction[3])
-        elif len(instruction) > 4:
-            value = instruction[4]
-            if len(instruction) > 5:
-                for i in range(5, len(instruction)):
-                    value = value + " " + instruction[i]
-            verb_dict[verb](instruction[1], instruction[2], instruction[3], value)
+        verb_dict[verb](instruction)
     else:
         print("{0} is not a programmed instruction.".format(instruction[0]))
 
@@ -49,7 +37,7 @@ def save_in_file():
 
 
 #----- display instruction ----
-def display(*args):
+def display(instruction):
     noun_dict = {
         "assignm": display_assignments,
         "subj": display_subj,
@@ -57,15 +45,15 @@ def display(*args):
         "display one": dislpay_one
     }
 
-    if len(args) == 0:
+    if len(instruction) == 0:
         display_all()
-    elif len(args) == 1:
-        if args[0] in noun_dict:
-            noun_dict[args[0]]()
+    elif len(instruction) == 1:
+        if instruction[0] in noun_dict:
+            noun_dict[instruction[0]]()
         else:
             print("Please use a valid noun")
     else:
-        noun_dict["display one"](args[0], args[1])
+        noun_dict["display one"](instruction[0], instruction[1])
 
 def display_all():
     i = 1
@@ -107,33 +95,25 @@ def display_in_order():
     for subj in school.get_subj_list():
         for assignm in subj.get_assignments():
             assignments.append(assignm)
-
-    for i in range(1, len(list)):
+    for i in range(1, len(assignments)):
         j = i
-        while j < len(list):
-            if list[i-1] > list[j]:
-                (list[i-1], list[j]) = (list[j], list[i-1])
+        while j < len(assignments):
+            if assignments[i-1].get_delivery_date() > assignments[j].get_delivery_date():
+                (assignments[i-1], assignments[j]) = (assignments[j], assignments[i-1])
                 j = i
             else:
                 j += 1
-    
     for assignm in assignments:
-        print(str(assignm.get_delivery_date()))
-    #assignments = []
-    #assignments_dates = []
-    #for subj in school.get_subj_list():
-    #    for assignm in subj.get_assignments():
-    #        assignments.append(assignm)
-    #        assignments_dates.append(assignm.get_delivery_date())
+        print("Delivery: " + str(assignm.get_delivery_date()) + " " + assignm.get_name())
 #----- display instruction ----
 
 #----- add instruction ----
-def add(*args):
+def add(instruction):
     noun_dict = {
         "hw": add_homework
     }
-    if args[0] in noun_dict:
-        noun_dict[args[0]](args[1])
+    if instruction[0] in noun_dict:
+        noun_dict[instruction[0]](instruction[1])
     else:
         print("Please use a valid noun")
 def add_homework(subj_index):
@@ -144,30 +124,47 @@ def add_homework(subj_index):
 #----- add instruction ----
 
 #----- edit instruction ----
-def edit(*args):
-    # noun, subj_index, assignm_index, value
+def edit(instruction):
+    noun = instruction[0]
+    del instruction[0]
     noun_dict = {
         "name": edit_name,
         "pcomp": edit_perc_completed,
-        "p1hr": edit_perc_1hr
+        "p1hr": edit_perc_1hr,
+        "delivery": edit_delivery_date
     }
-    if args[0] in noun_dict:
-        noun_dict[args[0]](args[1], args[2], args[3])
+    if noun in noun_dict:
+        noun_dict[noun](instruction)
     else:
         print("Please use a valid noun")
-def edit_name(subj_index, assignm_index, value):
+def edit_name(instruction):
+    subj_index = instruction[0]
+    assignm_index = instruction[1]
+    value = instruction[2]
     school.get_subj_list()[int(subj_index)-1].get_assignments()[int(assignm_index)-1].set_name(value)
     save_in_file()
-def edit_perc_completed(subj_index, assignm_index, value):
+def edit_perc_completed(instruction):
+    subj_index = instruction[0]
+    assignm_index = instruction[1]
+    value = instruction[2]
     school.get_subj_list()[int(subj_index)-1].get_assignments()[int(assignm_index)-1].set_completed(float(value))
     save_in_file()
-def edit_perc_1hr(subj_index, assignm_index, value):
+def edit_perc_1hr(instruction):
+    subj_index = instruction[0]
+    assignm_index = instruction[1]
+    value = instruction[2]
     school.get_subj_list()[int(subj_index)-1].get_assignments()[int(assignm_index)-1].set_perc_in1hr(float(value))
+    save_in_file()
+def edit_delivery_date(instruction):
+    subj_index = instruction[0]
+    assignm_index = instruction[1]
+    delivery_date = dt.date(dt.datetime.now().year, int(input("Month: ")), int(input("Day: ")))
+    school.get_subj_list()[int(subj_index)-1].get_assignments()[int(assignm_index)-1].set_delivery_date(delivery_date)
     save_in_file()
 #----- edit instruction ----
 
 #----- remove instruction ----
-def remove(*args):
+def remove(instruction):
     pass
 #----- remove instruction ----
 
@@ -204,7 +201,7 @@ while True:
 #                 j += 1
 #     return list
 
-    
+
 
 
 # TODO: set date of subject
